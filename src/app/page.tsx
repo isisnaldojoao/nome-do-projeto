a24ac9fc01dd0b45d636fe20df630d7795cb13ee
João Isisnaldo <145622770+isisnaldojoao@users.noreply.github.com>
2025-03-22 12:49:08 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
 import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
+
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Head>
+      <title>LOUDinhos</title>
+      <meta name="description" content="Site sobre a LOUD e seus conteúdos" />
+      <meta name="keywords" content="loud, loudgg, loudlol, loudvalorant" />
+      <meta property="og:title" content="LOUDinhos" />
+      <meta property="og:description" content="Site sobre a LOUD e seus conteúdos" />
+      <meta property="og:image" content="https://seusite.com/loudinhos_news.png" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://seusite.com" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="LOUDinhos" />
+      <meta name="twitter:description" content="Site sobre a LOUD e seus conteúdos" />
+      <meta name="twitter:image" content="https://seusite.com/loudinhos_news.png" />
+    </Head>
+
+      
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
